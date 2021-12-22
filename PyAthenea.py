c22fb0e846e2866e4135208c42cba00e771bb736
Carlos Castillo <73676436+crlscstll@users.noreply.github.com>
2021-12-22 14:37:09 -0500

    Update PyAthenea.py

--- PyAthenea.py
+++ PyAthenea.py
@@ -1,10 +1,9 @@
 import speech_recognition as sr
 import pyttsx3, pywhatkit
 
-name="athenea"
 listener = sr.Recognizer()
-engine = pyttsx3.init()
 
+engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[0].id)
 
@@ -12,28 +11,35 @@ def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-def listen():
-    try:
-        with sr.Microphone() as source:
-            print("Escuchando ... ")
-            pc = listener.listen(source)
-            rec = listener.recognize_google(pc)
-            rec= rec.lower()
-            if name in rec:
-                rec=rec.replace(name,'')
+def assist(command):
+    if 'athenea' in command:
+        command=command.replace('athenea','')
+        if 'reproduce' in command:
+            music= command.replace('reproduce','')
+            print("Reproduciendo"+ music)
+            talk("Reproduciendo"+music)
+            pywhatkit.playonyt(music)
+    elif 'atenea' in command:
+        command=command.replace('atenea','')
+        if 'reproduce' in command:
+            music= command.replace('reproduce','')
+            print("Reproduciendo"+ music)
+            talk("Reproduciendo"+music)
+            pywhatkit.playonyt(music)
 
 
-    except:
-        pass
-    return rec
 
-def run_athenea():
-    rec = listen()
-    if 'reproduce' in rec:
-        music= rec.replace('reproduce','')
-        print("Reproduciendo"+ music)
-        talk("Reproduciendo"+music)
-        pywhatkit.playonyt(music)
+#Escucha el comando si se nombra athenea o atena el comando lo pasa a la funcion assist
 
-if __name__ == '__main__':
-    run_athenea()
+try:
+    with sr.Microphone() as source:
+        print("Escuchando ... ")
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice, language = "es-MX")
+        command = command.lower()
+        if 'athenea' in command:
+            assist(command)
+        elif 'atenea' in command:
+            assist(command)
+except:
+    pass
